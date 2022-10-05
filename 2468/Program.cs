8b67ecf9627e8248fcc1d4042ec4f62e872780f4
Anna <kira8721@mail.ru>
2022-10-05 21:23:11 +0300

    2468

---
+++ 2468/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("*****************************\n Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("*****************************\n Введите второе число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+{
+if (n > a) 
+{ 
+Console.WriteLine("Первое число больше!");}
+else if (n < a ) Console.WriteLine("Второе число больше");
+else if (n == a ) Console.WriteLine("Числа равны, повторите попытку");
+}
