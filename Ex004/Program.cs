7c47e56f880000ed65e48176264233d372c0270a
Anna <kira8721@mail.ru>
2022-10-05 21:53:00 +0300

    004

---
+++ Ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("*****************************\n Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("*****************************\n Введите второе число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("*****************************\n Введите третье число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+{
+if ( n > a&n > b ) Console.WriteLine("Первое число максимальное"); 
+else if ( a > n&a > b ) Console.WriteLine("Второе число максимальное");
+else if (b > n&b > a ) Console.WriteLine("Третье число максимальное");
+}
