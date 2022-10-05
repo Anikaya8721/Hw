597d46bca5567ec81b28acee18b12a15e49b2c80
Anna <kira8721@mail.ru>
2022-10-05 22:04:56 +0300

    008

---
+++ Ex008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("*****************************\n Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = n;
+while(n>=1)
+{
+   if (n % 2 == 0)
+Console.WriteLine(Convert.ToString(n)+" ");
+n--; 
+}
