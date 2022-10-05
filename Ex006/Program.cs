48032024ca9538165f5cd49edac6751ea6b3ad6c
Anna <kira8721@mail.ru>
2022-10-05 21:57:38 +0300

    006

---
+++ Ex006/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("*****************************\n Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+Console.WriteLine("yes"); 
+}
+else
+{
+Console.Write("no");} 
