d4ebb00b74fed45b7d00780fc58fd44620fb9abc
Anna <kira8721@mail.ru>
2022-10-10 17:20:13 +0300

    Добавила папку Ex 015

--- Ex013/Program.cs
+++ Ex013/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+string n_text = Convert.ToString(n);
+
+if (n_text.Length > 2)
+{
+  Console.WriteLine("третья цифра: " + n_text[2]);
+}
+else
+{
+  Console.WriteLine("третьей цифры нет");
+}
