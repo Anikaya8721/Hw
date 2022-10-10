1cb55dba533e1bab00febe7efc79a2a62f579332
Anna <kira8721@mail.ru>
2022-10-10 17:13:40 +0300

    Добавила папку Ex 013

--- Ex010/Program.cs
+++ Ex010/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10 
+Console.WriteLine("Введите трёхзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100 && n < 999)
+{
+    int a = n / 10;
+    int lastDigit = a % 10;
+    Console.WriteLine("Вторая цифра: " + lastDigit);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
