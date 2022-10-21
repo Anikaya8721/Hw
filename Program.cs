0c6a8dacbcf18befc029eb64815b39771ea8d0b5
Anikaya8721 <113709863+Anikaya8721@users.noreply.github.com>
2022-10-21 14:09:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,64 +1,58 @@
-﻿//Задача 25
-/*Console.WriteLine("Введите первое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите степень первого числа: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int i;
-int sum = 1;
+﻿//Задача 34. Задайте массив заполненный случайными полож-ми 3-х значными числами.
+//Написать прогу, кот.покажет кол-во четных чисел в массиве.
+/*int[] array = new int[5];
+Random rand = new Random();
+int count = 0;
 
-for (i = 1; i <= m; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    sum = n * sum;
-}
-Console.WriteLine($"Число {n} в степени {m} равно = {sum}");*/
-
-//Задача 27
-/*Console.WriteLine("Введите первое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int num1 = 0;
-int res = 0;
-int sum = 0;
-
-while (n > 10)
-{
-    num1 = n % 10;
-    sum = sum + num1;
-    n = n / 10;
-}
-if (n<10)
-{
-    res = sum + n;
-}
-
-Console.WriteLine($"Сумма цифр во введенном числе {n} = {res}");*/
-
-//Задача 29
-/*void newArray (int[] array)
-{
-    int length = array.Length;
-    int i = 0;
-    while (i < length)
+    array[i] = rand.Next(100, 1000);
+    if (array[i] % 2 == 0)
     {
-        Console.WriteLine($"Введите элемент массива [{i}]:");
-        int n = Convert.ToInt32(Console.ReadLine());
-        array[i] = n;
-        i++;
+        count++;
     }
 }
-void PrintArray(int[] arr)
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(count);*/
+
+
+//Задача 36. Задать одномерный массив заполненный случ. числами. Найти сумму элем-ов, стоящих на нечетных позициях.
+
+/*int[] array = new int[8];
+Random rand = new Random();
+int n = 0;
+
+for (int i = 0; i < array.Length; i++)
 {
-    int count = arr.Length;
-    int i = 0;
-    while (i < count)
+    array[i] = rand.Next(-9, 10);
+}
+for (int i = 1; i < array.Length; i += 2)
+{
+    n = array[i] + n;
+}
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Сумма элементов array[1], [3], [5], [7] = {n}");*/
+
+
+//Задача 38. Задайте массив вещ-ых чисел. Найти разницу между макс. и мин-м элем-ов масива.
+/*double[] array = { 8, 2, 55, 90, 5 };
+double count = 0;
+double max = 0;
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
     {
-        Console.Write(arr[i] + " ");
-        i++;
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
     }
 }
-int[] array = new int[8];
-int count = array.Length;
-
-newArray(array);
-PrintArray(array);*/
-
+count = max - min;
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Минимальное число = {min}");
+Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом = {count}");*/
 
