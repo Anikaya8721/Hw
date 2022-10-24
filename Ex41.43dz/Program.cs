91ee287f8ce35a7305b4cadadd70b80514a8b39c
Anna <kira8721@mail.ru>
2022-10-24 16:39:36 +0300

    иооараа

--- Ex41.43dz/Program.cs
+++ Ex41.43dz/Program.cs
@@ -1,4 +1,49 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*Console.WriteLine("Введите M чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int i = 0;
+    while (i < length)
+    {
+        Console.WriteLine($"Введите число №{i + 1}:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        array[i] = n;
+        i++;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int i = 0;
+    while (i < count)
+    {
+        Console.Write(arr[i] + " ");
+        i++;
+    }
+}
+
+int[] array = new int[m];
+
+FillArray(array);
+Console.WriteLine("Вы ввели: ");
+PrintArray(array);
+int res = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        res += 1;
+    }
+}
+Console.WriteLine(" ");
+
+Console.WriteLine($"Чисел больше 0 = {res}");*/
 
 
 
@@ -8,7 +53,7 @@
 //  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("значения b1");
+/*Console.WriteLine("значения b1");
 double b1 = double.Parse(Console.ReadLine()!);
 Console.WriteLine("значения k1");
 double k1 = double.Parse(Console.ReadLine()!);
@@ -34,4 +79,4 @@ else
     {
         Console.WriteLine("прямые параллельны");
     }
-}
+}*/
